e8fdcb5778c0f3df82f8be1859cd446b6cc9d290
Fabricio Valadares <fabricio.valadares@gmail.com>
2021-02-16 11:44:36 -0300

    SelectDateFragment.kt partially implemented ===     * Cancel button OK - Navigation OK     * OK button sending a Toast

--- app/src/main/java/com/localizalabsacademy/mobile/rentacar/ui/SelectDateFragment.kt
+++ app/src/main/java/com/localizalabsacademy/mobile/rentacar/ui/SelectDateFragment.kt
@@ -1,16 +1,21 @@
 package com.localizalabsacademy.mobile.rentacar.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.localizalabsacademy.mobile.rentacar.R
+import com.localizalabsacademy.mobile.rentacar.databinding.FragmentSelectDateBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private const val TAG = "SelectDateFragment"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,27 +23,59 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SelectDateFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var binding: FragmentSelectDateBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_select_date, container, false)
+        val fragmentBinding = FragmentSelectDateBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.selectDateFragment = this
+    }
+
+
+    fun cancelAction() {
+        /**
+         * Destroy the fragment and return to [StartFragment]
+         */
+
+        findNavController().navigate(R.id.action_selectDateFragment_to_startFragment)
+        Toast.makeText(context, "Cancel button clicked", Toast.LENGTH_SHORT).show()
+        Log.d(TAG, "Cancel Action activated")
+    }
+
+    fun okButtonAction() {
+
+        Toast.makeText(context, "Okay button clicked", Toast.LENGTH_SHORT).show()
+        Log.d(TAG, "Okay Action activated")
+    }
+
+
+    /**
+     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
+     * is being removed. As a result, clear out the binding object.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 
     companion object {
+
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
