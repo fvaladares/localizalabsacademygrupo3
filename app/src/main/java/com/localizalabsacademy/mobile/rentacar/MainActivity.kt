db7d7bab9428119e7cb7ce941e2cba651fe38ccb
Fabricio Valadares <fabricio.valadares@gmail.com>
2021-02-15 21:55:34 -0300

    MainActivity.kt is now just an container.
    StartFragment.kt is the main layout.

--- app/src/main/java/com/localizalabsacademy/mobile/rentacar/MainActivity.kt
+++ app/src/main/java/com/localizalabsacademy/mobile/rentacar/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.localizalabsacademy.mobile.rentacar
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    /**
+     * It is responsible for control the fragments
+     */
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        this.setupActionBarWithNavController(navController)
+
+        Log.d(TAG, "Starting onCreate...")
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
---
+++ app/src/main/java/com/localizalabsacademy/mobile/rentacar/ui/StartFragment.kt
@@ -0,0 +1,52 @@
+package com.localizalabsacademy.mobile.rentacar.ui
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.localizalabsacademy.mobile.rentacar.databinding.FragmentStartBinding
+
+
+class StartFragment : Fragment() {
+    // Binding object instance corresponding to the fragment_start.xml layout
+    // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
+    // when the view hierarchy is attached to the fragment.
+    private var binding: FragmentStartBinding? = null
+//    private val sharedViewModel: OrderViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+//        Log.d(TAG, "onCreteView")
+        val fragmentBinding = FragmentStartBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, "onViewCreated")
+        binding?.startFragment = this
+    }
+
+
+    /**
+     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
+     * is being removed. As a result, clear out the binding object.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(TAG, "onDestroy")
+        binding = null
+    }
+
+
+    companion object {
+        private const val TAG = "StartFragment"
+    }
+}
