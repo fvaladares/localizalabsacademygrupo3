459883a6a66b891b7320ad9c70815218d69e76d8
Fabricio Valadares <fabricio.valadares@gmail.com>
2021-02-18 10:36:40 -0300

    Start activity -> Now the return field disappear when the return in the same pick-up location is checked.

--- app/src/main/java/com/localizalabsacademy/mobile/rentacar/MainActivity.kt
+++ app/src/main/java/com/localizalabsacademy/mobile/rentacar/MainActivity.kt
@@ -2,10 +2,12 @@ package com.localizalabsacademy.mobile.rentacar
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -13,8 +15,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
      * It is responsible for control the fragments
      */
     private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.d(TAG, "Starting onCreate...")
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
@@ -22,7 +27,30 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
         this.setupActionBarWithNavController(navController)
 
-        Log.d(TAG, "Starting onCreate...")
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home ->
+                    Toast.makeText(
+                        this,
+                        "Home",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                R.id.menu_my_reservation ->
+                    Toast.makeText(
+                        this,
+                        "Reservation",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                R.id.menu_profile -> Toast.makeText(
+                    this,
+                    "Profile",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            true
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
